f05a762fb641f4ba134634f375727bd193b6300b
Bokji <202145030@itc.ac.kr>
2024-10-22 15:17:30 +0900

    feat : 랜덤 수 3개 받는 class 생성

---
+++ src/main/java/baseball/User.java
@@ -0,0 +1,13 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+
+    public static void main(String[] args) {
+        // 유저가 입력한 3개의 숫자를 받기 (배열로?)
+        List<Integer> user = new ArrayList<>();
+
+    }
+}
